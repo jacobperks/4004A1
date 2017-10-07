108a41051aa2d1922a24dcf6c7b8961b6f8b0e6c
jacobperks <jacobperks@cmail.carleton.ca>
2017-10-07 11:46:14 -0400

    User table get and lookupUsername functions tests+code

---
+++ server/logic/tables/UserTable.java
@@ -0,0 +1,41 @@
+package server.logic.tables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import server.logic.model.User;
+
+public class UserTable {
+	List<User> userList=new ArrayList<User>();
+    private static class UserListHolder {
+        private static final UserTable INSTANCE = new UserTable();
+    }
+    private UserTable(){
+    	//set up the default list with some instances
+    	String[] passwordList=new String[]{"Zhibo","Yu","Michelle","Kevin","Sun"};
+    	String[] usernameList=new String[]{"Zhibo@carleton.ca","Yu@carleton.ca","Michelle@carleton.ca","Kevin@carleton.ca","Sun@carleton.ca"};
+    	for(int i=0;i<usernameList.length;i++){
+			User deuser=new User(i,usernameList[i],passwordList[i]);
+			userList.add(deuser);
+		}
+    };
+    public static final UserTable getInstance() {
+        return UserListHolder.INSTANCE;
+    }
+    
+	
+	public List<User> getUserTable() {
+		return userList;
+	}
+
+	public int lookup(String string) {
+		int userid=-1;
+		for(int i=0;i<userList.size();i++){
+			if(userList.get(i).getUsername().equalsIgnoreCase(string)){
+				userid=i;
+			}
+		}
+		return userid;
+	}
+}
---
+++ tests/unit/UserTableTest.java
@@ -0,0 +1,46 @@
+package tests.unit;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.model.User;
+import server.logic.tables.UserTable;
+
+public class UserTableTest {
+
+	UserTable userTable;
+	
+	@Before
+	public void setup() throws Exception {
+		userTable = UserTable.getInstance();
+	}
+	
+	@Test 
+	public void getUserTableTest() {
+		List<User> userListActual = userTable.getUserTable();
+    	String[] passwordList=new String[]{"Zhibo","Yu","Michelle","Kevin","Sun"};
+    	String[] usernameList=new String[]{"Zhibo@carleton.ca","Yu@carleton.ca","Michelle@carleton.ca","Kevin@carleton.ca","Sun@carleton.ca"};
+		for (int i = 0; i < userListActual.size(); i++) {
+			assertEquals(i, userListActual.get(i).getUserid());
+			assertEquals(usernameList[i], userListActual.get(i).getUsername());
+			assertEquals(passwordList[i], userListActual.get(i).getPassword());
+		}
+	}
+	
+	@Test
+	public void lookupUsernameTestPass() {
+		String[] usernameList=new String[]{"Zhibo@carleton.ca","Yu@carleton.ca","Michelle@carleton.ca","Kevin@carleton.ca","Sun@carleton.ca"};
+		for (int i = 0; i < usernameList.length; i++) {
+			assertEquals(i, userTable.lookup(usernameList[i]));
+		}
+	}
+	
+	@Test
+	public void lookupUsernameTestFail() {
+		assertEquals(-1, userTable.lookup("Jacob@carleton.ca"));
+	}
+}
