c799fbee34d8ce6fe1f012f4821798a3fa08eb95
jacobperks <jacobperks@cmail.carleton.ca>
2017-10-07 15:54:18 -0400

    OutputHandler createUser function tests+code

--- tests/unit/handler/OutputHandlerTest.java
+++ tests/unit/handler/OutputHandlerTest.java
@@ -2,18 +2,25 @@ package tests.unit.handler;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import server.logic.handler.model.Output;
 import server.logic.handler.OutputHandler;
+import server.logic.model.User;
+import server.logic.tables.UserTable;
 
 public class OutputHandlerTest {
 
 	OutputHandler outputHandler;
+	UserTable userTable;
 
 	@Before
 	public void setup() throws Exception {
 		outputHandler = new OutputHandler();
+		userTable = UserTable.getInstance();
 	}
 	
 	@Test
@@ -30,5 +37,28 @@ public class OutputHandlerTest {
 		assertEquals(true, outputHandler.isNumber("9781442668584"));
 		assertEquals(true, outputHandler.isNumber("2"));
 	}
-
+	
+	@Test
+	public void createUserPass() {
+		Output expected = new Output("Success!",2);
+		Output actual = outputHandler.createUser("jacob@carleton.ca,password123");
+		assertEquals(expected.getState(), actual.getState());
+		assertEquals(expected.getOutput(), actual.getOutput());
+		List<User> userListActual = userTable.getUserTable();
+		assertEquals("jacob@carleton.ca", userListActual.get(userListActual.size()-1).getUsername());
+		assertEquals("password123", userListActual.get(userListActual.size()-1).getPassword());
+	}
+	
+	@Test
+	public void createUserFail() {
+		List<User> userList = userTable.getUserTable();
+		for (int i = 0; i < userList.size(); i++){
+			Output expected = new Output("The User Already Exists!",2);
+			String username = userList.get(i).getUsername();
+			String password = userList.get(i).getPassword();
+			Output actual = outputHandler.createUser(username+","+password);
+			assertEquals(expected.getState(), actual.getState());
+			assertEquals(expected.getOutput(), actual.getOutput());
+		}
+	}
 }
