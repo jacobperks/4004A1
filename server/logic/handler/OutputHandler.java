4aefc931e3cba34d5ad0bc37a19da23611f0d624
jacobperks <jacobperks@cmail.carleton.ca>
2017-10-08 20:16:46 -0400

    All server/client code, InputHandler, and Trace added

--- server/logic/handler/OutputHandler.java
+++ server/logic/handler/OutputHandler.java
@@ -326,7 +326,7 @@ public class OutputHandler {
 	public Output clerkLogin(String input) {
 		Output output=new Output("",0);
 		if(input.equalsIgnoreCase(Config.CLERK_PASSWORD)){
-			output.setOutput("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.");
+			output.setOutput("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item,Monitor.");
         	output.setState(CLERK);
 		}else{
 			output.setOutput("Wrong Password!Please Input The Password:");
