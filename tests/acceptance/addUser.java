bddaa7628fa876451ded25283701d4498f08f460
jacobperks <jacobperks@cmail.carleton.ca>
2017-10-09 17:17:43 -0400

    addUser acceptance tests

---
+++ tests/acceptance/addUser.java
@@ -0,0 +1,72 @@
+package tests.acceptance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import server.logic.handler.InputHandler;
+import server.logic.handler.OutputHandler;
+import server.logic.handler.model.Output;
+import server.logic.handler.model.ServerOutput;
+
+public class addUser {
+
+	@Test
+	public void validTest1() {
+		String result = "";
+		InputHandler inputHandler = new InputHandler();
+		OutputHandler outputHandler = new OutputHandler();
+		Output o = new Output("",0);
+		String output = "";
+		ServerOutput oo = new ServerOutput(output,o.getState());
+		
+		oo = inputHandler.processInput("CLERK", InputHandler.CLERKLOGIN);
+		o = outputHandler.clerkLogin("admin");
+		
+		oo = inputHandler.processInput(o.getOutput(), o.getState());
+		
+		if (oo.getState() == InputHandler.CLERK) {
+			String input = "jacobperks,password123";
+			o = outputHandler.createUser(input);
+    		oo.setOutput(o.getOutput());
+            oo.setState(o.getState());
+            if (oo.getState() == InputHandler.CREATEUSER) {
+            	input = "jacob@carleton.ca,password123";
+    			o = outputHandler.createUser(input);
+        		oo.setOutput(o.getOutput());
+                oo.setState(o.getState());
+                result = o.getOutput();
+            }
+		} else {
+			result = "Failure!";
+		}
+		assertEquals("Success!", result);
+	}
+	
+	@Test
+	public void invalidTest2() {
+		String result = "";
+		InputHandler inputHandler = new InputHandler();
+		OutputHandler outputHandler = new OutputHandler();
+		Output o = new Output("",0);
+		String output = "";
+		ServerOutput oo = new ServerOutput(output,o.getState());
+		
+		oo = inputHandler.processInput("CLERK", InputHandler.CLERKLOGIN);
+		o = outputHandler.clerkLogin("admin");
+		
+		oo = inputHandler.processInput(o.getOutput(), o.getState());
+		
+		if (oo.getState() == InputHandler.CLERK) {
+			String input = "zhibo@carleton.ca,password123";
+			o = outputHandler.createUser(input);
+    		oo.setOutput(o.getOutput());
+            oo.setState(o.getState());
+            result = o.getOutput();
+		} else {
+			result = "Failure!";
+		}
+		assertEquals("The User Already Exists!", result);
+	}
+
+}
